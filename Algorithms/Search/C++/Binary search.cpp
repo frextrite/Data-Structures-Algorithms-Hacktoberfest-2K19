f27b9d06b08655e9b3711ff87f444392a552ff08
David Gokimmung <43725247+Davidgokimmung@users.noreply.github.com>
2019-10-01 22:58:40 +0530

    Update Binary search.cpp
    Used ternary operations instead of if-else conditions inside the function and documented the code.

--- Algorithms/Search/C++/Binary search.cpp
+++ Algorithms/Search/C++/Binary search.cpp
@@ -1,36 +1,23 @@
 #include <iostream>
 using namespace std;
-
-
-int binarySearch(int arr[], int l, int r, int x)
+int binarySearch(int arr[], int l, int r, int x) //Binary Search function to find element x in array arr[]
 {
     if (r >= l) {
-        int mid = l + (r - l) / 2;
-  
-
-        if (arr[mid] == x)
-            return mid;
-
-
-        if (arr[mid] > x)
-            return binarySearch(arr, l, mid - 1, x);
-
-
-        return binarySearch(arr, mid + 1, r, x);
+        int mid = l + (r - l) / 2; //selection of mid element
+        return (arr[mid]==x?mid:arr[mid]>x?binarySearch(arr, l, mid - 1, x):binarySearch(arr, mid + 1, r, x));
     }
-
-
     return -1;
 }
-
+//Main function
 int main(void)
 {
     int arr[50];
-    int x,len;
+    int x,len;//len stores the number of elements to be given as input
     cout<<"Enter length of list = ";
     cin>>len;
     cout<<"Enter array elements\n";
-    for(int i = 0; i < len; i++){
+    for(int i = 0; i < len; i++)//Taking array input
+    {
         cin>>arr[i];
     }
     cout<<"Enter element you want to find = ";
