5dc0c8ab70bb4a4754d3821cd476446c76c1a55d
kryptocodes <srivatsantb@gmail.com>
2019-10-02 18:38:00 +0530

    removed extra spaces

--- Algorithms/Sort/c/bubble_sort.c
+++ Algorithms/Sort/c/bubble_sort.c
@@ -14,19 +14,16 @@ for(c=0;c<n;c++)
    printf("%ld\n", array[c]);
 return 0;
 }
-void bubble_sort(long list[], long n)
+void bubble_sort(long list[],long n)
 {
-  long c, d, t;
-  for(c=0;c<n-1;c++)
-  {
-    for(d=0;d<n-c-1;d++)
-    {
-      if(list[d]>list[d+1])
-      {
-        t = list[d];
-        list[d]=list[d+1];
-        list[d+1]=t;
-      }
-    }
-  }
-}
+long c, d, t;
+for(c=0;c<n-1;c++)
+{
+for(d=0;d<n-c-1;d++)
+{
+if(list[d]>list[d+1])
+{
+t = list[d];
+list[d]=list[d+1];
+list[d+1]=t;
+}}}}
