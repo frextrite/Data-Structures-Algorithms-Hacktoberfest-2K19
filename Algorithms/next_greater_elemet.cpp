d6ce6923c096fdaa72e935a4aa2f1d67d194c53d
Sahil-Shende <47074843+Sahil-Shende@users.noreply.github.com>
2019-10-02 18:11:40 +0530

    algorithm for next greater element
    linear time algorithm

---
+++ Algorithms/next_greater_elemet.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+
+void printNGE(int arr[], int n) { 
+  stack < int > s; 
+  
+  
+  s.push(arr[0]); 
+
+  for (int i = 1; i < n; i++) { 
+  
+    if (s.empty()) { 
+      s.push(arr[i]); 
+      continue; 
+    } 
+
+    while (s.empty() == false && s.top() < arr[i]) 
+    {          
+        cout << s.top() << " --> " << arr[i] << endl; 
+        s.pop(); 
+    } 
+
+    s.push(arr[i]); 
+  } 
+
+  while (s.empty() == false) { 
+    cout << s.top() << " --> " << -1 << endl; 
+    s.pop(); 
+  } 
+} 
+
+int main() 
+{ 
+    cout << "Enter the length of array";
+    int n;
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elemets";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    printNGE(arr, n); 
+    return 0; 
+}
