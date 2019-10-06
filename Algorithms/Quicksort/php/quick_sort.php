3798b727bb56405c2260ce6f46fbb37803c22d86
Guillermo Laseca <guillelaseca@gmail.com>
2019-10-07 00:38:35 +0200

    Add quick sort in PHP

---
+++ Algorithms/Quicksort/php/quick_sort.php
@@ -0,0 +1,23 @@
+<?php
+
+function quick_sort($array){
+	$left = $right = array();
+	if(count($array) < 2){
+		return $array;
+	}
+	$pivot_key = key($array);
+	$pivot = array_shift($array);
+	foreach($array as $val){
+		if($val <= $pivot){
+			$left[] = $val;
+		}elseif($val > $pivot){
+			$right[] = $val;
+		}
+	}
+	return array_merge(quick_sort($left),array($pivot_key=>$pivot),quick_sort($right));
+}
+ 
+$array = array(4, 3, 0, 8, -1, 5, 1);
+echo 'Original array : '.implode(',',$array).'\n';
+$sorted_array = quick_sort($array);
+echo 'Sorted array : '.implode(',',$sorted_array);
