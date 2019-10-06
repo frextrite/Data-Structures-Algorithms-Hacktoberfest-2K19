5fa2af4c38850d3cfa7476cb2f0cad9817c05647
somyamahajan18 <somyamahajan18@gmail.com>
2019-10-06 15:12:23 +0530

    Fixed a typo

--- Algorithms/next_greater_element.cpp
+++ Algorithms/next_greater_element.cpp
@@ -1,47 +1,47 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-  
+#include <bits/stdc++.h>
+using namespace std;
 
-void printNGE(int arr[], int n) { 
-  stack < int > s; 
-  
-  
-  s.push(arr[0]); 
 
-  for (int i = 1; i < n; i++) { 
-  
-    if (s.empty()) { 
-      s.push(arr[i]); 
-      continue; 
-    } 
+void printNGE(int arr[], int n) {
+  stack < int > s;
 
-    while (s.empty() == false && s.top() < arr[i]) 
-    {          
-        cout << s.top() << " --> " << arr[i] << endl; 
-        s.pop(); 
-    } 
 
-    s.push(arr[i]); 
-  } 
+  s.push(arr[0]);
 
-  while (s.empty() == false) { 
-    cout << s.top() << " --> " << -1 << endl; 
-    s.pop(); 
-  } 
-} 
+  for (int i = 1; i < n; i++) {
 
-int main() 
-{ 
+    if (s.empty()) {
+      s.push(arr[i]);
+      continue;
+    }
+
+    while (s.empty() == false && s.top() < arr[i])
+    {
+        cout << s.top() << " --> " << arr[i] << endl;
+        s.pop();
+    }
+
+    s.push(arr[i]);
+  }
+
+  while (s.empty() == false) {
+    cout << s.top() << " --> " << -1 << endl;
+    s.pop();
+  }
+}
+
+int main()
+{
     cout << "Enter the length of array";
     int n;
     cin >> n;
     int arr[n];
-    cout << "Enter the elemets";
+    cout << "Enter the elements";
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
-    
-    printNGE(arr, n); 
-    return 0; 
+
+    printNGE(arr, n);
+    return 0;
 }
