7e96a9a73cb925f6bf20db23531cb17dc28adae3
Mehul1604 <mathurmehul3@gmail.com>
2019-10-07 01:30:24 +0530

    I wrote an Algo for finding the inverse of a matrix using determinants

--- Algorithms/Matrix_Inverse/Inverse.c
+++ Algorithms/Matrix_Inverse/Inverse.c
@@ -1,154 +1,34 @@
 #include<stdio.h>
+ 
+int main(){
+ 
+  int arr[3][3],row,col;
+  float det=0;
+ 
+  //inputting the matrix
+  for(row=0;row<3;row++)
+      for(col=0;col<3;col++)
+           scanf("%d",&arr[row][col]);
+ 
+  
+ 
+  for(row=0;row<3;row++)
 
+      det = det + (arr[0][row]*(arr[1][(row+1)%3]*arr[2][(row+2)%3] - arr[1][(row+2)%3]*arr[2][(row+1)%3]));
 
+  if(det==0)
+  printf("Inverse does not exist!");
 
+  else
+  {
+ 
+   //printing inverse
+   for(row=0;row<3;row++){
+      for(col=0;col<3;col++)
+           printf("%.2f\t",((arr[(row+1)%3][(col+1)%3] * arr[(row+2)%3][(col+2)%3]) - (arr[(row+1)%3][(col+2)%3]*arr[(row+2)%3][(col+1)%3]))/ det);
+       printf("\n");
+   }
 
-
-int main()
-{
-
-
-int row,col,arr[3][6];
-
-//the original matrix
-
-	for(row=0;row<3;row++)
-	{
-	
-		for(col=0;col<3;col++)
-		{
-		
-			scanf("%d",&arr[row][col]);
-		
-		
-		}
-
-		printf("\n");
-	
-	
-	}
-
-	//identity element on the right
-
-	for(row=0;row<3;row++)
-	{
-	
-		for(col=3;col<6;col++)
-		{
-		
-			if((col-3)==row)
-			   arr[row][col]=1;
-
-			else
-			   arr[row][col]=0;
-		
-		
-		}
-	
-	
-	
-	
-	}
-
-	float rat;
-	int k=0;
-	int l,m,temp,sel;
-
-
-	for(col=0;col<3;col++)
-	{
-	
-		for(row=0;row<3;row++)
-		{
-		
-
-		if(arr[col][col]==0)
-		{
-		
-			for(k=col+1;k<3;k++)
-			{
-			
-				if(arr[k][col]!=0)
-					sel=k;
-			
-			
-			}
-
-			for(k=0;k<6;k++)
-			{
-			
-				
-				
-					temp=arr[col][k];
-					arr[col][k]=arr[sel][k];
-					arr[sel][k]=temp;
-				
-				
-			
-			
-			
-			}
-		
-		
-		
-		}
-
-		if(!(row==col))
-		{
-		
-			rat=(float)(arr[row][col])/arr[col][col];	
-		
-		
-		}
-
-		for(k=0;k<6;k++)
-		{
-		
-			arr[row][k]=arr[row][k]-rat*arr[col][k];
-		
-		
-		
-		}
-		
-		
-		}
-	
-	
-	
-	
-	}
-
-
-
-	for(k=0,l=3,m=0;k<3,l<6,m<3;m++,l++,k++)
-	{
-		arr[k][l]=(float)(arr[m][l])/arr[k][k];
-	
-	
-	}
-
-	//printing inverted matrix
-	
-	for(row=0;row<3;row++)
-	{
-	
-		for(col=3;col<6;col++)
-		{
-		
-			printf("%.3f  ",(float)arr[row][col]);
-		
-		
-		}
-
-		printf("\n");
-	
-	
-	}
-
-
-
-
-
-
-
+ 	}
+   return 0;
 }
